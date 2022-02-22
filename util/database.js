126ea5d7c79e5201aa6db8bb48f1a98ca8335eef
OMEGA <leon.wai@live.com>
2022-02-22 23:09:59 +0630

    change database

--- util/database.js
+++ util/database.js
@@ -1,16 +1,16 @@
 const Sequelize = require("sequelize")
 
 // postgres
-// const sequelize = new Sequelize('postgres://crdjlxbo:OMBMA35GyD6ukmLs73_u88edBVOR3tUz@john.db.elephantsql.com/crdjlxbo')
+const sequelize = new Sequelize('postgres://crdjlxbo:OMBMA35GyD6ukmLs73_u88edBVOR3tUz@john.db.elephantsql.com/crdjlxbo')
 
 // mysql
-const sequelize = new Sequelize(
-    "thinzarphyu",
-    "root",
-    "wailinhtet",
-    {
-        host: "localhost",
-        dialect: "mysql",
-    })
+// const sequelize = new Sequelize(
+//     "thinzarphyu",
+//     "root",
+//     "wailinhtet",
+//     {
+//         host: "localhost",
+//         dialect: "mysql",
+//     })
 
 module.exports = sequelize
